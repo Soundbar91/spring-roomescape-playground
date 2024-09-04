7a90817be219d00d18e6fbdd6eeb4162a085bb19
신관규 <soundbar91@gmail.com>
2024-09-04 21:32:28 +0900

    feat: 7단계 기능 구현
    예약정보 생성 및 삭제 JDBC 적용

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,16 +1,16 @@
 package roomescape.controller;
 
 import java.net.URI;
+import java.sql.PreparedStatement;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Objects;
-import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,8 +29,6 @@ import roomescape.mapper.ReservationMapper;
 @RestController
 public class ReservationController {
 
-    private final List<Reservation> reservations = new ArrayList<>();
-    private final AtomicLong index = new AtomicLong(1);
     private final ReservationMapper reservationMapper;
     private JdbcTemplate jdbcTemplate;
 
@@ -43,12 +41,25 @@ public class ReservationController {
     public ResponseEntity<ResponseReservation> createReservation(
         @Valid @RequestBody RequestCreateReservation requestCreateReservation
     ) {
-        Reservation reservation = reservationMapper.toEntity(index.getAndIncrement(), requestCreateReservation);
-        reservations.add(reservation);
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(
+                "INSERT INTO RESERVATION(name, date, time) VALUES (?, ?, ?)",
+                new String[]{"id"});
+            ps.setString(1, requestCreateReservation.name());
+            ps.setString(2, requestCreateReservation.date().toString());
+            ps.setString(3, requestCreateReservation.time().toString());
+
+            return ps;
+        }, keyHolder);
+
+        Reservation reservation = reservationMapper.toEntity(keyHolder.getKey().longValue(), requestCreateReservation);
+        ResponseReservation responseReservation = reservationMapper.toResponse(reservation);
 
         return ResponseEntity
-            .created(URI.create("/reservations/" + reservation.getId()))
-            .body(reservationMapper.toResponse(reservation));
+            .created(URI.create("/reservations/" + keyHolder.getKey().longValue()))
+            .body(responseReservation);
     }
 
     @GetMapping
@@ -69,12 +80,11 @@ public class ReservationController {
     public ResponseEntity<Void> deleteReservation(
         @PathVariable Long id
     ) {
-        Reservation reservation = reservations.stream()
-            .filter(r -> Objects.equals(r.getId(), id))
-            .findFirst()
-            .orElseThrow(() -> new NoSuchElementException("존재하지 않는 예약 정보입니다"));
+        final String sql = "DELETE FROM RESERVATION WHERE id = ?";
+        if (jdbcTemplate.update(sql, id) == 0) {
+            throw new NoSuchElementException("존재하지 않는 예약 정보입니다");
+        }
 
-        reservations.remove(reservation);
         return ResponseEntity.noContent().build();
     }
 }
